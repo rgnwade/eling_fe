acf3ece4e90ecac31357cef706f6a9c4326c8481
FOREST\regina.ayu <regina.wade93@gmail.com>
2023-01-20 22:28:11 +0700

    no message

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \FleetCart\Http\Middleware\TrustProxies::class,
         \FleetCart\Http\Middleware\RedirectToInstallerIfNotInstalled::class,
         \FleetCart\Http\Middleware\RunUpdater::class,
-        \FleetCart\Http\Middleware\getTokenUser::class,
+        // \FleetCart\Http\Middleware\getTokenUser::class,
     ];
 
     /**
